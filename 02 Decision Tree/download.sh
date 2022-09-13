284dd3d580259f0bc270d741f7ca6953f95bce05
Kloud <kloud80@gmail.com>
2022-09-14 02:08:46 +0900

    IDE 안내

--- 02 Decision Tree/download.sh
+++ 02 Decision Tree/download.sh
@@ -1,9 +1,7 @@
-FILE=$1
-echo "Specified [$FILE]"
-URL=[$FILE]
-ZIP_FILE=./data.zip
-TARGET_DIR=./data/
+URL=$1
+ZIP_FILE=data.zip
+TARGET_DIR=data/
 wget -N $URL -O $ZIP_FILE
 mkdir $TARGET_DIR
-unzip $ZIP_FILE -d ./datasets/
+unzip $ZIP_FILE -d data/
 rm $ZIP_FILE
